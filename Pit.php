157299b48e778f0dae3f6b7437c3d6c107bcb1d0
ha1t <halt.feits@gmail.com>
2010-11-20 18:15:12 +0900

    first commit

---
+++ Pit.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * Pit
+ *
+ */
+
+require_once 'spyc/spyc.php';
+
+/**
+ * Pit
+ *
+ */
+class Pit
+{
+    var $version = '1.0.1';
+    var $directory = '~/.pit/';
+    var $config = 'pit.yaml';
+    var $profile = 'default.yaml';
+
+    /**
+     * Pit
+     *
+     * @access public
+     */
+    function Pit()
+    {
+        if (strpos($this->directory, '~') === 0) {
+            $this->directory = str_replace('~', getenv('HOME'), $this->directory);
+        }
+
+        if (!is_dir($this->directory)) {
+            mkdir($this->directory);
+        }
+        
+        if (!is_file($this->directory . $this->config)) {
+            touch($this->directory . $this->config);
+            chmod($this->directory . $this->config, 0600);
+        }
+        
+        if (!is_file($this->directory . $this->profile)) {
+            touch($this->directory . $this->profile);
+            chmod($this->directory . $this->profile, 0600);
+        }
+    }
+
+    /**
+     * set
+     *
+     */
+    function set($name, $options = array())
+    {
+        if (isset($options['data'])) {
+            $result = $options['data'];
+        } else {
+            if (isset($options['config'])) {
+                $yaml = Spyc::YAMLDump($options['config']);
+            } else {
+                $config_data = $this->get($name);
+                var_dump($config_data);exit();
+                $yaml = Spyc::YAMLDump($config_data);
+            }
+
+            $tfilename = tempnam(sys_get_temp_dir(), 'pit');
+
+            $tfh = fopen($tfilename, 'w+');
+            fwrite($tfh, $yaml);
+            fclose($tfh);
+
+            $ph = popen(getenv("EDITOR") . ' ' . $tfilename, 'w');
+            pclose($ph);
+
+            $result = file_get_contents($tfilename);
+
+            if ($result == $config_data) {
+                //No Changes
+            }
+
+            //yaml 2 array
+            $result = Spyc::YAMLLoad($tfilename);
+            unset($result[0]);
+            unlink($tfilename);
+        }
+
+        //@todo implement load
+        $config = $this->load();
+        $config[$name] = $result;
+
+        file_put_contents($this->directory . $this->profile, Spyc::YAMLDump($config));
+
+        return $config[$name];
+    }
+
+    function get($name, $options = array())
+    {
+        $load_data = $this->load();
+        $result = $load_data[$name];
+
+        if (!is_array($result)) {
+            $result = array();
+        }
+
+        if ((count($result) == 0) && isset($options['require'])) {
+            foreach ($options['require'] as $key => $item) {
+                if (isset($result[$key])) {
+                } else {
+                    $result[$key] = $item;
+                }
+            }
+            $result = $this->set($name, array('config' => $result));
+        }
+
+        if (count($result) == 0) {
+            return array('username' => '', 'password' => '');
+        } else {
+            return $result;
+        }
+    }
+
+    function switchProfile($name, $options = array())
+    {
+        $this->profile = $name . '.yaml';
+        $config = $this->config();
+        $config['profile'] = $name;
+
+        file_put_contents($this->directory . $this->config, Spyc::YAMLDump($config));
+    }
+
+    function load()
+    {
+        $config = $this->config();
+        $this->switchProfile($config['profile']);
+        $yaml = Spyc::YAMLLoad($this->directory . $this->profile);
+        unset($yaml[0]);
+        return $yaml;
+    }
+
+    function config()
+    {
+        $yaml = Spyc::YAMLLoad($this->directory . $this->config);
+        unset($yaml[0]);
+        return $yaml;
+    }
+
+}
+
+/*
+$pit = new Pit();
+$re = $pit->get('none', array('require' =>
+    array(
+        'mail' => 'your mail',
+        'pass' => 'your pass'
+    )
+));
+var_dump($re);
+ */
+?>
